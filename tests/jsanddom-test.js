03a0a500c498c994ab43e4c23f7fb28fa861f5a3
sejalpatel2 <sejalpatel2@hotmail.com>
2015-08-01 23:27:40 -0400

    Update jsanddom-test.js

--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -92,5 +92,58 @@ test("isOnlyWhitespace Test", 3, function() {
      equal(isOnlyWhitespace('   \t\r  \n   \t \n   '), true, 'Check with only white space characters'); 
      equal(isOnlyWhitespace('   \t\r  fn   \t \n   '), false, 'Check with regular characters'); 
 }); 
-
-
+/********************************
+Closure Test.............
+********************************/ 
+test("Closure Test", 6, function() { 
+     equal(typeof MyClosure, 'function', 'Verify MyClosure constructor'); 
+     var my1 = MyClosure(42); 
+     var my2 = MyClosure('different value'); 
+     equal(my1.privateFunction, undefined, 'Verify our function is private'); 
+     equal(my1.privateProperty, undefined, 'Verify our property is private'); 
+     equal(my1.answerToLife(), 42, 'Verify public method works with passed in value'); 
+     equal(my1.getPrivate(), 'private', 'Verify our access to the private property works'); 
+     equal(my2.answerToLife(), 'different value', 'Make sure our class objects are unique'); 
+}); 
+/********************************
+Test.............
+********************************/ 
+test("DataTable Test", 7, function(){ 
+     equal(typeof DataTable, 'function', 'Verify DataTable constructor'); 
+     var table = DataTable(); 
+     // Check private properties 
+     equal(table.columns, undefined, 'Check columns is private'); 
+     equal(table.rows, undefined, 'Check rows is private'); 
+     // Check publis methods 
+     equal(typeof table.addColumns, 'function', 'Check addColumns is public method'); 
+     equal(typeof table.addRow, 'function', 'Check addRow is public method'); 
+     equal(typeof table.getData, 'function', 'Check getData is public method'); 
+     // Add our data 
+     table.addColumns('column1', 'column2', 'column3'); 
+     table.addRow('value1A', 'value1B', 'value1C'); 
+     table.addRow('value2A', 'value2B', 'value2C'); 
+     var result = [{ 
+         column1: 'value1A', 
+         column2: 'value1B', 
+         column3: 'value1C' 
+     }, { 
+         column1: 'value2A', 
+         column2: 'value2B', 
+         column3: 'value2C' 
+ }]; 
+     deepEqual(table.getData(), result, 'Check that we get valid table object'); 
+}); 
+/********************************
+Test.............
+********************************/ 
+test("Footer link selectors", 16, function() { 
+     equal(typeof getFooterCheckAllAnchor, 'function', 'Verify getFooterCheckAllAnchor function is present'); 
+     var selectors = getFooterCheckAllAnchor(),i, sel; 
+     for(i=1; i<=selectors.length; i++) 
+     { 
+         sel = selectors[i-1]; 
+         equal(sel.length, 2, 'Check selector ' + i + ' has two anchors'); 
+         equal(sel[0].id, 'check', 'Check selector ' + i + ' has "Check All" anchor in first position'); 
+         equal(sel[1].id, 'uncheck', 'Check selector ' + i + ' has "Uncheck All" anchor in second position'); 
+     } 
+}); 
